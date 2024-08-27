07c70f0b32208682213742b66d7946da2ecf90ba
OkyRuiz <159487334+OkyRuiz@users.noreply.github.com>
2024-08-27 09:33:26 -0500

    Cambios en Jugador y Portero

--- src/main/java/futbol/Jugador.java
+++ src/main/java/futbol/Jugador.java
@@ -2,50 +2,55 @@ package futbol;
 
 public class Jugador extends Futbolista{
 	
+	 //ATRIBUTOS
 	 public short golesMarcados;
-	    public byte dorsal;
+	 public byte dorsal;
+	 
+	 //CONSTRUCTOR SIN PARÁMETROS
+	 public Jugador() {
+		 this.golesMarcados = 289;
+	     this.dorsal = 7;
+	     
+	 }
 
-	    public Jugador() {
-	        this.golesMarcados = 289;
-	        this.dorsal = 7;
-	    }
+	 public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal) {
+		 super(nombre, edad, posicion);
+	     this.golesMarcados = golesMarcados;
+	     this.dorsal = dorsal;
+	     }
+	 
+	 //SOBRE ESCRIBIR EL MÉTODO ABSTRACTO
+	 @Override
+	 public boolean jugarConLasManos() {
+		 return false;
+		 }
 
-	    public Jugador(String nombre, int edad, String posicion, short golesMarcados, byte dorsal) {
-	        super(nombre, edad, posicion);
-	        this.golesMarcados = golesMarcados;
-	        this.dorsal = dorsal;
-	    }
+	 @Override
+	 public int compareTo(Object f) {
+		 Jugador p=(Jugador) f;
+	     return Math.abs(this.getEdad()-p.getEdad());
+	     }
 
-	    @Override
-	    public boolean jugarConLasManos() {
-	        return false;
+	 @Override
+	 public String toString() {
+		 return "El futbolista " +getNombre()+ " tiene " +getEdad()+ ", y juega de " +getPosicion()+" con el dorsal "+dorsal+". Ha marcado "+golesMarcados;
 	    }
+	 
+	 //MÉTODOS SET Y GET
+	 public short getGolesMarcados() {
+		 return golesMarcados;
+		 }
 
-	    @Override
-	    public int compareTo(Object f) {
-	        Jugador p=(Jugador) f;
-	        return Math.abs(this.getEdad()-p.getEdad());
-	    }
+	 public void setGolesMarcados(short golesMarcados) {
+		 this.golesMarcados = golesMarcados;
+		 }
 
-	    @Override
-	    public String toString() {
-	        return "El futbolista " +getNombre()+ " tiene " +getEdad()+ ", y juega de " +getPosicion()+" con el dorsal "+dorsal+". Ha marcado "+golesMarcados;
-	    }
+	 public byte getDorsal() {
+		 return dorsal;
+		}
 
-	    public short getGolesMarcados() {
-	        return golesMarcados;
-	    }
-
-	    public void setGolesMarcados(short golesMarcados) {
-	        this.golesMarcados = golesMarcados;
-	    }
-
-	    public byte getDorsal() {
-	        return dorsal;
-	    }
-
-	    public void setDorsal(byte dorsal) {
-	        this.dorsal = dorsal;
-	    }
+	 public void setDorsal(byte dorsal) {
+		 this.dorsal = dorsal;
+		 }
 
 }
--- src/main/java/futbol/Portero.java
+++ src/main/java/futbol/Portero.java
@@ -2,44 +2,49 @@ package futbol;
 
 public class Portero extends Futbolista{
 	
+	 //ATRIBUTOS
 	 public short golesRecibidos;
-	    public byte dorsal;
+	 public byte dorsal;
+	 
+     //CONSTRUCTOR
+	 public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+		 super(nombre, edad, "Portero");
+	     this.golesRecibidos = golesRecibidos;
+	     this.dorsal = dorsal;
+	     }
+     
+	 //SOBRE ESCRIBIR EL MÉTODO ABSTRACTO
+	 @Override
+	 public boolean jugarConLasManos() {
+		 return true;
+		 }
 
-	    public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
-	        super(nombre, edad, "Portero");
-	        this.golesRecibidos = golesRecibidos;
-	        this.dorsal = dorsal;
+	 @Override
+	 public String toString() {
+		 return "El futbolista " +getNombre()+ " tiene " +getEdad()+ ", y juega de " +getPosicion()+" con el dorsal "+dorsal+". Le han marcado "+golesRecibidos;
 	    }
 
-	    @Override
-	    public boolean jugarConLasManos() {
-	        return true;
-	    }
+	 @Override
+	 public int compareTo(Object f) {
+		 Portero p=(Portero) f;
+		 return Math.abs(this.golesRecibidos-p.golesRecibidos);
+		 }
+	 
+	 //MÉTODOS SET Y GET
 
-	    @Override
-	    public String toString() {
-	        return "El futbolista " +getNombre()+ " tiene " +getEdad()+ ", y juega de " +getPosicion()+" con el dorsal "+dorsal+". Le han marcado "+golesRecibidos;
-	    }
+	 public short getGolesRecibidos() {
+		 return golesRecibidos;
+		 }
 
-	    @Override
-	    public int compareTo(Object f) {
-	        Portero p=(Portero) f;
-	        return Math.abs(this.golesRecibidos-p.golesRecibidos);
-	    }
+	 public void setGolesRecibidos(short golesRecibidos) {
+		 this.golesRecibidos = golesRecibidos;
+		 }
 
-	    public short getGolesRecibidos() {
-	        return golesRecibidos;
-	    }
+	 public byte getDorsal() {
+		 return dorsal;
+		 }
 
-	    public void setGolesRecibidos(short golesRecibidos) {
-	        this.golesRecibidos = golesRecibidos;
-	    }
-
-	    public byte getDorsal() {
-	        return dorsal;
-	    }
-
-	    public void setDorsal(byte dorsal) {
-	        this.dorsal = dorsal;
-	    }
+	 public void setDorsal(byte dorsal) {
+		 this.dorsal = dorsal;
+		 }
 }
