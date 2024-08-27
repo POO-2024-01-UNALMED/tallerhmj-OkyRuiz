895e62b1c4791aa9c2ecf44d163fb719a90c8fd8
OkyRuiz <159487334+OkyRuiz@users.noreply.github.com>
2024-08-27 09:17:48 -0500

    DETALLES

--- src/main/java/futbol/Futbolista.java
+++ src/main/java/futbol/Futbolista.java
@@ -1,34 +1,41 @@
 package futbol;
 
 public abstract class Futbolista implements Comparable{
+	
+	//ATRIBUTOS
     private String nombre;
     private int edad;
     private final String posicion;
 
+    //CONSTRUCTOR SIN PARÁMETROS 
     public Futbolista() {
         this.nombre="Maradona";
         this.posicion="delantero";
         this.edad=30;
     }
-
+    
+    //CONSTRUCTOR CON PARÁMETROS 
     public Futbolista(String nombre,int edad, String posicion) {
         this.nombre=nombre;
         this.edad=edad;
         this.posicion=posicion;
 
     }
-
-    public boolean equals(Futbolista p) {
-        return this==p;
+    
+    //COMPARAR SI DOS FUTBOLSITAS SON IGUALES
+    public boolean equals(Futbolista f) {
+        return this == f;
     }
-
+    
+    //MÉTODO ABSTRACTO
     abstract public boolean jugarConLasManos();
 
     @Override
     public String toString() {
         return "El futbolista " +nombre+ " tiene " +edad+ ", y juega de " +posicion;
     }
-
+    
+    //MÉTODOS SET Y GET
     public String getNombre() {
         return nombre;
     }
