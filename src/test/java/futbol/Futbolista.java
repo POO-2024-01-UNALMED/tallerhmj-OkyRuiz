40a997f84a6ab19a573c341b8f3de15654d32d2c
OkyRuiz <159487334+OkyRuiz@users.noreply.github.com>
2024-08-27 01:11:04 -0500

    Creación de las clases

---
+++ src/test/java/futbol/Futbolista.java
@@ -0,0 +1,63 @@
+package futbol;
+
+public abstract class Futbolista implements Comparable<Futbolista> {
+	
+	//ATRIBUTOS
+	private String nombre;
+	private int edad;
+	private final String posicion;
+	
+	//CONSTRUCTOR CON PARÁMETROS
+	public Futbolista(String nombre, int edad, String posicion) {
+		this.nombre = nombre;
+		this.edad=edad;
+		this.posicion=posicion;
+		
+	}
+	
+	//CONSTRUCTOR POR DEFECTO
+	 public Futbolista() {
+		 this("Maradona", 30, "delantero");
+	 }
+	 
+	 //EL MÉTODO TOSTRING
+	 public String toString() {
+		 return "El futbolista " + nombre + " tiene " + edad 
+				 + ", y juega de " + posicion;
+	 }
+	 
+	 //MÉTODO PARA COMPARAR SI DOS FUTBOLISTAS SON IGUALES
+	 public boolean equals(Futbolista f) {	 
+		 boolean retorno = false;
+		 
+		 if (this ==f) {
+			 retorno = true;
+		 }
+		 
+		 return retorno;
+	 }
+	 
+	 //MÉTODO ABSTRACTO 
+	 public abstract boolean jugarConLasManos();
+	 
+	// MÉTODOS GET Y SET
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    public int getEdad() {
+	        return edad;
+	    }
+
+	    public void setEdad(int edad) {
+	        this.edad = edad;
+	    }
+
+	    public String getPosicion() {
+	        return posicion;
+	    } 
+}
---
+++ src/test/java/futbol/Portero.java
@@ -0,0 +1,37 @@
+package futbol;
+
+public class Portero extends Futbolista{
+	
+	//ATRIBUTOS 
+	public short golesRecibidos;
+	public byte dorsal;
+	
+    // CONSTRUCTOR CON PARÁMETROS
+    public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+        super(nombre, edad, "Portero");
+        this.golesRecibidos = golesRecibidos;
+        this.dorsal = dorsal;
+    }
+    
+    // MÉTODO compareTo
+    @Override
+    public int compareTo(Futbolista f) {
+        if (f instanceof Portero) {
+            return Math.abs(this.golesRecibidos - ((Portero) f).golesRecibidos);
+        } else {
+            throw new ClassCastException("No se puede comparar un Portero con un Jugador");
+        }
+    }
+    
+    // MÉTODO toString
+    @Override
+    public String toString() {
+        return super.toString() + " con el dorsal " + dorsal + ". Le han marcado " + golesRecibidos + " goles";
+    }
+    
+	
+	//SOBRE ESCRIBIR EL MÉTODO ABSTRACTO
+	public boolean jugarConLasManos() {
+		return true;
+	}
+}
